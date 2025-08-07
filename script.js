cb665db7a8ea631508b9c1529e39cb77421a49cc
Guilherme <guilhermesilvacoelho01@gmail.com>
2025-08-07 04:15:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-document.getElementById('confete').addEventListener('click', function (e) {
+document.getElementById('confete').addEventListener('click', function () {
     const btn = document.getElementById('confete');
     const rect = btn.getBoundingClientRect();
+
     const x = (rect.left + rect.width / 2) / window.innerWidth;
     const y = (rect.top + rect.height / 2) / window.innerHeight;
 
     confetti({
       particleCount: 100,
-      spread: 70,
+      spread: 360,        
+      startVelocity: 25,  
+      scalar: 1.2,       
+      ticks: 60,         
       origin: { x: x, y: y }
     });
   });
