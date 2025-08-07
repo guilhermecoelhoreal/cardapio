1888841c03b1b1c2f426080c041a7c6a222cc07c
Guilherme <guilhermesilvacoelho01@gmail.com>
2025-08-07 04:02:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.getElementById('confete').addEventListener('click', function () {
+document.getElementById('confete').addEventListener('click', function (e) {
+    const btn = document.getElementById('confete');
+    const rect = btn.getBoundingClientRect();
+    const x = (rect.left + rect.width / 2) / window.innerWidth;
+    const y = (rect.top + rect.height / 2) / window.innerHeight;
+
     confetti({
       particleCount: 100,
       spread: 70,
-      origin: { y: 0.6 }
+      origin: { x: x, y: y }
     });
   });
